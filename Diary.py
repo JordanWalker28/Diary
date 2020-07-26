0281f3bffb0af2a302e692d6cecf3793fde27f00
Jordan Walker <jordanswalker28@gmail.com>
2020-07-26 22:42:31 +0100

    Added simple interface

--- Diary.py
+++ Diary.py
@@ -1,5 +1,8 @@
 import pathlib
 import datetime
+import tkinter as tk
+
+root= tk.Tk()
 
 def writeToFile(entry):
     file = pathlib.Path("diary.txt")
@@ -15,8 +18,24 @@ def writeToFile(entry):
     return
 
 def makeEntry():
-    entry = input()
+    entry = entry1.get()
     writeToFile(entry)
+
+
+canvas1 = tk.Canvas(root, width = 400, height = 300,  relief = 'raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Daily Notes')
+label1.config(font=('helvetica', 14))
+canvas1.create_window(200, 25, window=label1)
+
+
+entry1 = tk.Entry (root)
+canvas1.create_window(150, 140, window=entry1)
+
+
     
-while True:
-    makeEntry()
+button1 = tk.Button(text='Add', command=makeEntry, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+canvas1.create_window(150, 280, window=button1)
+
+root.mainloop()
